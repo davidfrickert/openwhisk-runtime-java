42781af26063991098f0c7e10ecbfa5a9c893f49
dfrickert <david.frickert@premium-minds.com>
2021-06-12 21:02:53 +0100

    example minio request in comments

--- test-app/filehashing-graal/src/main/java/ch/ethz/systems/FileHashingGraal.java
+++ test-app/filehashing-graal/src/main/java/ch/ethz/systems/FileHashingGraal.java
@@ -57,6 +57,10 @@ public class FileHashingGraal {
 																				XmlParserException,
 																				InternalException {
 		final var buffer = new byte[size];
+		/*
+		Request{method=GET, url=http://10.147.18.27:8999/files?location=, headers=[Host:10.147.18.27:8999, Accept-Encoding:identity, User-Agent:MinIO (Linux; amd64) minio-java/dev, Content-MD5:1B2M2Y8AsgTpgAmY7PhCfg==, x-amz-content-sha256:e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855, x-amz-date:20210610T171938Z, Authorization:AWS4-HMAC-SHA256 Credential=minio/20210610/us-east-1/s3/aws4_request, SignedHeaders=content-md5;host;x-amz-content-sha256;x-amz-date, Signature=1427e44b517bda011563c0fda359bd1338c5f33b5fc328a9030fc46b708b9487]}
+		Request{method=GET, url=http://10.147.18.27:8999/files/file-1.dat, headers=[Host:10.147.18.27:8999, Accept-Encoding:identity, User-Agent:MinIO (Linux; amd64) minio-java/dev, Content-MD5:1B2M2Y8AsgTpgAmY7PhCfg==, x-amz-content-sha256:e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855, x-amz-date:20210610T171938Z, Authorization:AWS4-HMAC-SHA256 Credential=minio/20210610/us-east-1/s3/aws4_request, SignedHeaders=content-md5;host;x-amz-content-sha256;x-amz-date, Signature=b57b17c16f8b6479aab1daf6e5d5c7900110898766c25dd7e4cedbf6a0f1e731]}
+		 */
 		try (InputStream stream = minioClient.getObject(GetObjectArgs.builder()
 																	 .bucket("files")
 																	 .object(String.format("file-%d.dat", seed))
