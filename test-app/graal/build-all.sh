a832aa78e8065b5fd6cb0f68d28c5329da6da14b
David Frickert <david.frickert@protonmail.com>
2021-12-01 13:58:19 +0000

    create scripts to build all functions jars - graal needs to be deployed onto maven repo - regular photons dont since we create the function with the jar

---
+++ test-app/graal/build-all.sh
@@ -0,0 +1,6 @@
+mvn clean package deploy -f filehashing-graal/pom.xml
+mvn clean package deploy -f iconify-graal/pom.xml
+mvn clean package deploy -f image-classification-graal/pom.xml
+mvn clean package deploy -f login-graal/pom.xml
+mvn clean package deploy -f sleep-graal/pom.xml
+mvn clean package deploy -f video-graal/pom.xml
---
+++ test-app/java/build-all.sh
@@ -0,0 +1,6 @@
+mvn clean package -f filehashing/pom.xml
+mvn clean package -f iconify/pom.xml
+mvn clean package -f image-classification/pom.xml
+mvn clean package -f login/pom.xml
+mvn clean package -f sleep/pom.xml
+mvn clean package -f video/pom.xml
